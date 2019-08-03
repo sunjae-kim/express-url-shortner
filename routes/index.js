03ad0dce3fb51f9479d077cdd5bf158889e7107f
sunjae-kim <kimsj9484@gmail.com>
2019-08-03 21:57:26 +0900

    Implement Delete url logic
     - Handle exceptions
     - Implement Delete url by shotened keyword

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import shortener from './shortener';
 const route = express.Router();
 
-route.post('/shorten', shortener.shortenUrl)
-route.get('/:path', shortener.toShortenedUrl)
+route.post('/shorten', shortener.shortenUrl);
+route.get('/:path', shortener.toShortenedUrl);
+route.delete('/:path', shortener.deleteShrotenedUrl);
 
 export default route;
--- routes/shortener.js
+++ routes/shortener.js
@@ -3,19 +3,44 @@ import { joi } from '../service';
 const { Shortener } = Model;
 
 const shortenUrl = async (req, res) => {
-    const { value, error } = joi.validateUrl(req.body)
-    if (error) return res.status(400).send(error);
-    const shortened = await Shortener.create(value);
-    res.send(shortened);
+    try {
+        const { value, error } = joi.validateUrl(req.body)
+        if (error) return res.status(400).send(error);
+        const shortened = await Shortener.create(value);
+        res.send(shortened);
+    } catch (error) {
+        res.send(error)
+    }
 }
 
 const toShortenedUrl = async (req, res) => {
-    const { path } = req.params;
-    const { from } = await Shortener.findOne({ where: { to: path } });
-    if (!from) return res.status(404);
-    res.redirect(from);
+    try {
+        const { path } = req.params;
+        const { from } = await Shortener.findOne({ where: { to: path } });
+        if (!from) return res.status(404);
+        res.redirect(from);
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+const deleteShrotenedUrl = async (req, res) => {
+    try {
+        const { path } = req.params;
+        const shorteneds = await Shortener.findAll({ where: { to: path } });
+        if (!shorteneds) return res.status(404);
+        for (const shortened of shorteneds) {
+            console.log(shortened)
+            await Shortener.destroy({ where: { id: shortened.id } });
+        };
+        res.send({ message: 'Deleted' });
+    } catch (error) {
+        res.send(error)
+    }
 }
 
 export default {
-    shortenUrl, toShortenedUrl
+    shortenUrl,
+    toShortenedUrl,
+    deleteShrotenedUrl,
 };
