4b093a9d8cc42bcda8f25a1c3e86bbc533717559
sunjae-kim <kimsj9484@gmail.com>
2019-08-03 17:31:17 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+// use helmet
+app.use(helmet());
+
+// use cors
+app.use(cors());
+
+// use morgan
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+
+// parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// life check route
+app.get('/', (req, res) => {
+  res.send('Server alive');
+});
+
+// connect route
+app.use('/', routes);
+
+// start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+export default app;
